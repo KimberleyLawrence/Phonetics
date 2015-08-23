f9726caad084beb8aad82ca87deb6e9d8d1678c7
KimberleyLawrence <kimberleynedic@gmail.com>
2015-08-23 14:52:46 -0700

    Still attempting to create a gallery

--- type.js
+++ type.js
@@ -2,23 +2,21 @@ angular.module('demo1', ['bootstrapLightbox']);
 angular.module('demo1').controller('GalleryCtrl',
   function($scope, Lightbox) {
   $scope.images = [
-    {"url": "https://farm6.staticflickr.com/5830/20552523531_e1efec8d49_k.jpg",
+    {'url': 'https://farm6.staticflickr.com/5830/20552523531_e1efec8d49_k.jpg',
     'thumbUrl': 'https://farm6.staticflickr.com/5830/20552523531_ef720cd2f1_s.jpg',
     'caption': 'This image has dimensions 2048x1519 and the img element is scaled to fit inside the window.'
+  },
+    { 'url': 'http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg',
+      'thumbUrl': 'http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg'
     },
-    {
-      'url': "http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg",
-      'thumbUrl': "http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg"
-    },
-    {'url': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_b.jpg', 'thumbUrl': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_s.jpg',
+    { 'url': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_b.jpg',
+    'thumbUrl': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_s.jpg',
       'caption': 'The left and right arrow keys are binded for navigation. The escape key for closing the modal is binded by AngularUI Bootstrap.'
     },
-    {
-      'url': 'https://farm1.staticflickr.com/260/20185156095_912c2714ef_b.jpg',
+    { 'url': 'https://farm1.staticflickr.com/260/20185156095_912c2714ef_b.jpg',
       'thumbUrl': 'https://farm1.staticflickr.com/260/20185156095_912c2714ef_s.jpg'
     },
-    {
-      'url': 'https://farm6.staticflickr.com/5757/20359334789_57316968ed_m.jpg',
+    { 'url': 'https://farm6.staticflickr.com/5757/20359334789_57316968ed_m.jpg',
       'thumbUrl': 'https://farm6.staticflickr.com/5757/20359334789_57316968ed_s.jpg',
       'caption': 'Default minimum modal dimensions (400x200) apply for this image (240x95).'
     },
