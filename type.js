040fd40e0049236372336f311beebba6927d79bd
KimberleyLawrence <kimberleynedic@gmail.com>
2015-08-22 22:03:09 -0700

    Fixing errors

--- type.js
+++ type.js
@@ -1,7 +1,8 @@
 angular.module('demo1', ['bootstrapLightbox']);
-angular.module('demo1').controller('GalleryCtrl', function ($scope, Lightbox) {
+angular.module('demo1').controller('GalleryCtrl',
+  function($scope, Lightbox) {
   $scope.images = [
-    {'url': 'https://farm6.staticflickr.com/5830/20552523531_e1efec8d49_k.jpg',
+    {"url": "https://farm6.staticflickr.com/5830/20552523531_e1efec8d49_k.jpg",
     'thumbUrl': 'https://farm6.staticflickr.com/5830/20552523531_ef720cd2f1_s.jpg',
     'caption': 'This image has dimensions 2048x1519 and the img element is scaled to fit inside the window.'
     },
@@ -9,9 +10,7 @@ angular.module('demo1').controller('GalleryCtrl', function ($scope, Lightbox) {
       'url': "http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg",
       'thumbUrl': "http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg"
     },
-    {
-      'url': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_b.jpg',
-      'thumbUrl': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_s.jpg',
+    {'url': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_b.jpg', 'thumbUrl': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_s.jpg',
       'caption': 'The left and right arrow keys are binded for navigation. The escape key for closing the modal is binded by AngularUI Bootstrap.'
     },
     {
